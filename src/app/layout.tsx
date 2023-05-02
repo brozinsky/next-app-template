bac83dff023cf48ae5f79b1a6accb810618848f6
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2023-05-02 19:52:16 +0200

    refactor(dark-switch): add persistent theme state using next-theme library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
-// import "./globals.css";
-
+import {Providers} from "./providers";
 import "@/styles/main.scss";
 
 export const metadata = {
@@ -9,8 +8,11 @@ export const metadata = {
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
